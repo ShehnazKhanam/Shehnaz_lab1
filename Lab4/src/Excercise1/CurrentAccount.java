261ce5747bbee52054e907e8e3fe4bfb68e91d19
ShehnazKhanam <73287678+ShehnazKhanam@users.noreply.github.com>
2020-10-29 19:40:50 +0530

    Add files via upload

---
+++ Lab4/src/Excercise1/CurrentAccount.java
@@ -0,0 +1,10 @@
+package Excercise1;
+
+public class CurrentAccount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Lab4/src/Excercise1/TestAccount.java
@@ -0,0 +1,10 @@
+package Excercise1;
+
+public class TestAccount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
