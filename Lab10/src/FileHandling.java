8ea8487d1667c4797a01ce566d05915f9648aabf
ShehnazKhanam <73287678+ShehnazKhanam@users.noreply.github.com>
2020-11-08 19:03:10 +0530

    Add files via upload

---
+++ Lab10/src/FileHandling.java
@@ -0,0 +1,18 @@
+//Desc:Code to check whether we can read,write a file 
+import java.io.*;
+public class FileHandling {
+	public static void main(String args[])
+	{
+		File f=new File("D://Shehnaz.txt");
+		String name=f.getName();
+		boolean b=f.isFile();//checks whether is a file or not
+		boolean w=f.canWrite();//Check whether we can write
+		boolean r=f.canRead();//Checks whether it is readable or not
+		long length=f.length();//Finds length
+		//String x=f.;
+		System.out.println(name+" "+b+" "+w+" "+r+" "+length);
+		
+		
+	}
+
+}
